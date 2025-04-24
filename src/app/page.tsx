94fb219fb89a8cb80a7e9fd2653196789f877105
hjp845 <tlfgql@dgist.ac.kr>
2025-04-24 16:42:51 +0900

    black inputtext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,12 +24,13 @@ export default function Home() {
       <div className="max-w-2xl mx-auto bg-white rounded-xl shadow-md p-6">
       <h1 className="text-2xl font-bold mb-4 text-black">맞춤법 검사기3</h1>
 
-        <textarea
-          className="w-full h-40 border p-2 rounded resize-none placeholder-gray-600"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="검사할 문장을 입력하세요"
-        />
+      <textarea
+        className="w-full h-40 border p-2 rounded resize-none placeholder-gray-600 text-black"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="검사할 문장을 입력하세요"
+      />
+
 
         <button
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
@@ -38,7 +39,7 @@ export default function Home() {
           검사하기
         </button>
         {result && (
-          <div className="mt-6 bg-gray-600 p-4 rounded border">
+          <div className="mt-6 bg-gray-100 p-4 rounded border">
             <h2 className="font-semibold mb-2">결과:</h2>
             <p>{result}</p>
           </div>
